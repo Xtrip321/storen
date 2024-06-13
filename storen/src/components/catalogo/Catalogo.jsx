9ea67ee514a3a8ee6537516ff0630d7050c750ab
Emmanuel <emma27.f.d@gmail.com>
2024-06-12 23:49:53 -0300

    Componente catalogo base terminado

--- storen/src/components/catalogo/Catalogo.jsx
+++ storen/src/components/catalogo/Catalogo.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import '../../styles/Catalogo.css'; // Asegúrate de importar tus estilos
-import img1 from '../../assets/imgs/Modelos/Venture/MAIN_1.png';
-import img2 from '../../assets/imgs/Modelos/Sungs/MAIN_1.png';
+import '../../styles/catalogo/Catalogo.css'; // Asegúrate de importar tus estilos
+import ProductCard from './ProductCard';
+import { models } from '../../data/modelData'; // Ajusta la ruta según sea necesario
+
 
 const Catalog = () => {
-    // Supongamos que tienes algunos productos en un array
-    const products = [
-        { id: 1, name: 'Producto 1', price: '20$', imageUrl: img1 },
-        { id: 2, name: 'Producto 2', price: '25$', imageUrl: img2 },
-        // Agrega más productos según sea necesario
-    ];
-
     return (
         <div className="catalog-container">
-            {products.map(product => (
-                <div key={product.id} className="product-card">
-                    <img src={product.imageUrl} alt={product.name} className="product-image" />
-                    <div className="product-info">
-                        <h3>{product.name}</h3>
-                        <p>{product.price}</p>
-                    </div>
-                </div>
+            {models.map((product, index) => (
+                <ProductCard key={index} product={product} />
             ))}
         </div>
     );
---
+++ storen/src/components/catalogo/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../styles/catalogo/ProductCard.css'; // Asegúrate de que el archivo CSS esté correctamente vinculado
+
+
+const ProductCard = ({ product }) => {
+    return (
+        <div className="product-card">
+            <img src={product.imageUrl} alt={product.name} className="product-image" />
+            <h3 className="product-title">{product.name}</h3>
+            <div className="color-options">
+                {product.colors.map((color, index) => (
+                    <img key={index} src={color.imageUrl} alt={color.name} className="color-image" />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
